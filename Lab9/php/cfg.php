f2ddc0ebc5ad3ca3775dfbe05a8f487e999bdaf4
DamianTodorowski <115210660+DamianTodorowski@users.noreply.github.com>
2024-12-10 11:31:26 +0100

    Add files via upload

---
+++ Lab9/php/cfg.php
@@ -0,0 +1,23 @@
+<?php
+// Plik konfiguracyjny do nawiązywania połączenia z bazą danych
+
+// Parametry połączenia z bazą danych
+$dbhost = 'localhost'; // Adres hosta bazy danych
+$dbuser = 'root';      // Nazwa użytkownika bazy danych
+$dbpass = '';          // Hasło do bazy danych
+$dbname = 'moja_strona'; // Nazwa bazy danych
+
+// Zmienna globalna dla połączenia
+global $link;
+
+// Nawiązanie połączenia z bazą danych
+$link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// Sprawdzenie, czy połączenie zostało nawiązane
+if (!$link) {
+    die('<b>Przerwane połączenie:</b> ' . mysqli_connect_error()); // Wyświetlenie błędu w przypadku problemu
+}
+
+// Komunikat potwierdzający połączenie
+echo '<b>Połączono z bazą danych.</b>';
+?>
---
+++ Lab9/php/plik.php
@@ -0,0 +1,4 @@
+<?php
+
+echo 'To tekst z include  <br /><br />';
+?>
